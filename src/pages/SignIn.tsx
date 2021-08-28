c21c8981d8facc6a12caf658e5a0e943d0a3207c
Irina <sidoren-irina@yandex.ru>
2021-08-28 22:07:27 +0300

    add theme, SignIn

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    fontWeight: 700
+  },
+  lightBulb: {
+    verticalAlign: 'middle',
+    marginRight: theme.spacing(1)
+  }
+}))
+
+function SignIn() {
+  return (
+    <div>
+      <div></div>>
+    </div>
+  )
+}
+
+export default SignIn
