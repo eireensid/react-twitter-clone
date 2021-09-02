4381f3f78d173dbf913d774e847fe40c01aa8b5d
Irina <sidoren-irina@yandex.ru>
2021-09-03 00:43:19 +0300

    modalBlock done

--- src/components/ModalBlock.tsx
+++ src/components/ModalBlock.tsx
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import { useStylesSignIn } from '../pages/SignIn';
 
-interface ModalProps {
+interface ModalBlockProps {
   title: string;
   children: React.ReactNode;
+  classes?: ReturnType<typeof useStylesSignIn>;
+  visible?: boolean;
+  onClose: () => void
 }
 
-export const Modal: React.FC<ModalProps> = ({ title, children }): React.ReactElement => {
-  const [open, setOpen] = useState(false)
-
-  const handleClickOpen = () => {
-    setOpen(true)
-  }
-
-  const handleClose = () => {
-    setOpen(false)
+export const ModalBlock: React.FC<ModalBlockProps> = ({
+  title,
+  onClose,
+  visible = false,
+  children,
+}: ModalBlockProps): React.ReactElement | null => {
+  if (!visible) {
+    return null;
   }
 
   return (
-    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+    <Dialog open={visible} onClose={onClose} aria-labelledby="form-dialog-title">
       <DialogTitle id="form-dialog-title">
-        <IconButton onClick={handleClickOpen} color="secondary" aria-label="close">
+        <IconButton onClick={onClose} color="secondary" aria-label="close">
           <CloseIcon style={{ fontSize: 26 }} color="secondary" />
         </IconButton>
         {title}
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles, Typography, Button } from '@material-ui/core';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import SearchIcon from '@material-ui/icons/Search';
 import PeopleIcon from '@material-ui/icons/PeopleOutline';
 import MessageIcon from '@material-ui/icons/ModeCommentOutlined';
+import { ModalBlock } from '../components/ModalBlock'
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
 
-const useStyles = makeStyles((theme) => ({
+
+
+export const useStylesSignIn = makeStyles((theme) => ({
   wrapper: {
     display: 'flex',
     height: '100vh'
@@ -72,11 +78,35 @@ const useStyles = makeStyles((theme) => ({
   rightBlockText: {
     fontWeight: 700,
     marginBottom: 15
-  }
+  },
+  loginField: {
+    marginBottom: 18,
+  },
+  registerField: {
+    marginBottom: theme.spacing(5),
+  },
+  formControl: {
+    marginBottom: theme.spacing(2),
+  },
+
 }))
 
 function SignIn() {
-  const classes = useStyles()
+  const classes = useStylesSignIn()
+
+  const [visibleModal, setVisibleModal] = useState<'signIn' | 'signUp'>()
+
+  const handleClickOpenSignIn = (): void => {
+    setVisibleModal('signIn')
+  }
+
+  const handleClickOpenSignUp = (): void => {
+    setVisibleModal('signUp')
+  }
+
+  const handleCloseModal = (): void => {
+    setVisibleModal(undefined)
+  }
 
   return (
     <section className={classes.wrapper}>    
@@ -102,8 +132,94 @@ function SignIn() {
           <TwitterIcon className={classes.rightBlockTwitterIcon} color="primary" />
           <Typography className={classes.rightBlockTitle} variant="h4">Узнайте, что происходит в мире прямо сейчас</Typography>
           <Typography className={classes.rightBlockText}>Присоединяйтесь к Твиттеру прямо сейчас</Typography>
-          <Button style={{ marginBottom: 20 }} variant="contained" color="primary" fullWidth>Зарегистрироваться</Button>
-          <Button variant="outlined" color="primary" fullWidth>Войти</Button>
+          <Button onClick={handleClickOpenSignUp} style={{ marginBottom: 20 }} variant="contained" color="primary" fullWidth>Зарегистрироваться</Button>
+          <Button onClick={handleClickOpenSignIn} variant="outlined" color="primary" fullWidth>Войти</Button>
+          <ModalBlock 
+            visible={visibleModal === 'signUp'} 
+            onClose={handleCloseModal} 
+            title="Создайте учетную запись">
+            <FormControl className={classes.formControl} component="fieldset" fullWidth>
+              <FormGroup aria-label="position" row>
+                <TextField
+                  className={classes.registerField}
+                  autoFocus
+                  id="name"
+                  label="Имя"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="filled"
+                  type="name"
+                  fullWidth
+                />
+                <TextField
+                  className={classes.registerField}
+                  autoFocus
+                  id="email"
+                  label="E-Mail"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="filled"
+                  type="email"
+                  fullWidth
+                />
+                <TextField
+                  className={classes.registerField}
+                  autoFocus
+                  id="password"
+                  label="Пароль"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="filled"
+                  type="password"
+                  fullWidth
+                />
+                <Button variant="contained" color="primary" fullWidth>
+                  Далее
+                </Button>
+              </FormGroup>
+            </FormControl>
+
+          </ModalBlock>
+
+          <ModalBlock 
+            visible={visibleModal === 'signIn'} 
+            onClose={handleCloseModal}  
+            title="Войти в аккаунт">
+            <FormControl className={classes.formControl} component="fieldset" fullWidth>
+              <FormGroup aria-label="position" row>
+                <TextField
+                  className={classes.loginField}
+                  autoFocus
+                  id="email"
+                  label="E-Mail"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="filled"
+                  type="email"
+                  fullWidth
+                />
+                <TextField
+                  className={classes.loginField}
+                  autoFocus
+                  id="password"
+                  label="Пароль"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="filled"
+                  type="password"
+                  fullWidth
+                />
+                <Button onClick={handleCloseModal} variant="contained" color="primary" fullWidth>
+                  Войти
+                </Button>
+              </FormGroup>
+            </FormControl>
+          </ModalBlock>
         </div>
       </div>
     </section>
