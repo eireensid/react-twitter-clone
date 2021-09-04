61070d9cae5f741cc108046a417f36ff9f7e724c
Irina <sidoren-irina@yandex.ru>
2021-09-04 22:51:57 +0300

    AddTweetForm add

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -6,7 +6,12 @@ import MessageIcon from '@material-ui/icons/EmailOutlined';
 import BookmarkIcon from '@material-ui/icons/BookmarkBorderOutlined';
 import ListIcon from '@material-ui/icons/ListAltOutlined';
 import UserIcon from '@material-ui/icons/PermIdentityOutlined';
-import { Button, IconButton, Typography } from '@material-ui/core';
+import CreateIcon from '@material-ui/icons/Create';
+import Hidden from '@material-ui/core/Hidden';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
 
 import { useHomeStyles } from '../pages/Home';
 
@@ -29,59 +34,77 @@ export const SideMenu: React.FC<SideMenuProps> = ({
         <li className={classes.sideMenuListItem}>
           <div>
             <SearchIcon className={classes.sideMenuListItemIcon} />
-            <Typography className={classes.sideMenuListItemLabel} variant="h6">
-              Поиск
-            </Typography>
+            <Hidden smDown>
+              <Typography className={classes.sideMenuListItemLabel} variant="h6">
+                Поиск
+              </Typography>
+            </Hidden>
+
           </div>
         </li>
         <li className={classes.sideMenuListItem}>
           <div>
             <NotificationIcon className={classes.sideMenuListItemIcon} />
-            <Typography className={classes.sideMenuListItemLabel} variant="h6">
-              Уведомления
-            </Typography>
+            <Hidden smDown>
+              <Typography className={classes.sideMenuListItemLabel} variant="h6">
+                Уведомления
+              </Typography>
+            </Hidden>
           </div>
         </li>
         <li className={classes.sideMenuListItem}>
           <div>
             <MessageIcon className={classes.sideMenuListItemIcon} />
-            <Typography className={classes.sideMenuListItemLabel} variant="h6">
-              Сообщения
-            </Typography>
+            <Hidden smDown>
+              <Typography className={classes.sideMenuListItemLabel} variant="h6">
+                Сообщения
+              </Typography>
+            </Hidden>
           </div>
         </li>
         <li className={classes.sideMenuListItem}>
           <div>
             <BookmarkIcon className={classes.sideMenuListItemIcon} />
-            <Typography className={classes.sideMenuListItemLabel} variant="h6">
-              Закладки
-            </Typography>
+            <Hidden smDown>
+              <Typography className={classes.sideMenuListItemLabel} variant="h6">
+                Закладки
+              </Typography>
+            </Hidden>
           </div>
         </li>
         <li className={classes.sideMenuListItem}>
           <div>
             <ListIcon className={classes.sideMenuListItemIcon} />
-            <Typography className={classes.sideMenuListItemLabel} variant="h6">
-              Список
-            </Typography>
+            <Hidden smDown>
+              <Typography className={classes.sideMenuListItemLabel} variant="h6">
+                Список
+              </Typography>
+            </Hidden>
           </div>
         </li>
         <li className={classes.sideMenuListItem}>
           <div>
             <UserIcon className={classes.sideMenuListItemIcon} />
-            <Typography className={classes.sideMenuListItemLabel} variant="h6">
-              Профиль
-            </Typography>
+            <Hidden smDown>
+              <Typography className={classes.sideMenuListItemLabel} variant="h6">
+                Профиль
+              </Typography>
+            </Hidden>
           </div>
         </li>
+        <li className={classes.sideMenuListItem}>
+          <Button
+            className={classes.sideMenuTweetButton}
+            variant="contained"
+            color="primary"
+            fullWidth>
+            <Hidden smDown>Твитнуть</Hidden>
+            <Hidden mdUp>
+              <CreateIcon />
+            </Hidden>
+          </Button>
+        </li>
       </ul> 
-      <Button
-        className={classes.sideMenuTweetButton}
-        variant="contained"
-        color="primary"
-        fullWidth>
-        Твитнуть
-      </Button>
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Container, Grid, Typography, Paper, makeStyles, Theme, withStyles, InputBase, createStyles } from '@material-ui/core';
+import classNames from 'classnames';
+import { Container, Grid, Typography, InputAdornment, Paper, makeStyles, Theme, withStyles, TextField, Button } from '@material-ui/core';
 import grey from '@material-ui/core/colors/grey';
+import SearchIcon from '@material-ui/icons/SearchOutlined';
+import PersonAddIcon from '@material-ui/icons/PersonAddOutlined';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import ListItem from '@material-ui/core/ListItem/ListItem';
+import Divider from '@material-ui/core/Divider/Divider';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar/Avatar';
+import ListItemText from '@material-ui/core/ListItemText/ListItemText';
+import List from '@material-ui/core/List/List';
+
 import { Tweet } from '../components/Tweet';
 import { SideMenu } from '../components/SideMenu';
+import { AddTweetForm } from '../components/AddTweetForm';
 
 
 export const useHomeStyles = makeStyles((theme: Theme) => ({
@@ -16,10 +28,12 @@ export const useHomeStyles = makeStyles((theme: Theme) => ({
     fontSize: 36
   },
   sideMenuList: {
+    position: 'sticky',
+    top: 0,
     listStyle: 'none',
     padding: 0,
     margin: 0,
-    width: 230
+    maxWidth: 230
   },
   sideMenuListItem: {
     cursor: 'pointer',
@@ -76,6 +90,7 @@ export const useHomeStyles = makeStyles((theme: Theme) => ({
     }
   },
   tweet: {
+    display: 'flex',
     cursor: 'pointer',
     paddingTop: 15,
     paddingLeft: 20,
@@ -85,7 +100,8 @@ export const useHomeStyles = makeStyles((theme: Theme) => ({
   },
   tweetAvatar: {
     width: theme.spacing(5),
-    height: theme.spacing(5)
+    height: theme.spacing(5),
+    marginRight: 15
   },
   tweetFooter: {
     display: 'flex',
@@ -96,36 +112,143 @@ export const useHomeStyles = makeStyles((theme: Theme) => ({
   },
   tweetUserName: {
     color: grey[500]
+  },
+  rightSide: {
+    paddingTop: 20,
+    position: 'sticky',
+    top: 0,
+  },
+  rightSideBlock: {
+    backgroundColor: '#F5F8FA',
+    borderRadius: 15,
+    marginTop: 20,
+    '& .MuiList-root': {
+      paddingTop: 0,
+    },
+  },
+  rightSideBlockHeader: {
+    borderTop: 0,
+    borderLeft: 0,
+    borderRight: 0,
+    backgroundColor: 'transparent',
+    padding: '13px 18px',
+    '& b': {
+      fontSize: 20,
+      fontWeight: 800,
+    },
+  },
+  rightSideBlockItem: {
+    cursor: 'pointer',
+    '& .MuiTypography-body1': {
+      fontWeight: 700,
+    },
+    '& .MuiListItemAvatar-root': {
+      minWidth: 50,
+    },
+    '& .MuiListItemText-root': {
+      margin: 0,
+    },
+    '&:hover': {
+      backgroundColor: '#edf3f6',
+    },
+  },
+  addForm: {
+    padding: 20,
+  },
+  addFormBody: {
+    display: 'flex',
+    width: '100%',
+  },
+  addFormBottom: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  addFormBottomActions: {
+    marginTop: 10,
+    paddingLeft: 70,
+  },
+  addFormTextarea: {
+    width: '100%',
+    border: 0,
+    fontSize: 20,
+    outline: 'none',
+    fontFamily: 'inherit',
+    resize: 'none',
+  },
+  addFormBottomLine: {
+    height: 12,
+    backgroundColor: '#E6ECF0',
+  },
+  addFormCircleProgress: {
+    position: 'relative',
+    width: 20,
+    height: 20,
+    margin: '0 10px',
+    '& .MuiCircularProgress-root': {
+      position: 'absolute',
+    },
+  },
+  addFormBottomRight: {
+    display: 'flex',
+    alignItems: 'center',
   }
 
 }))
 
-const SearchTextField = withStyles(() =>
-  createStyles({
-    input: {
+export const SearchTextField = withStyles((theme: Theme) => ({
+  root: {
+    '& .MuiOutlinedInput-root': {
       borderRadius: 30,
       backgroundColor: '#E6ECF0',
-      height: 45,
       padding: 0,
+      paddingLeft: 15,
+      '&.Mui-focused': {
+        backgroundColor: '#fff',
+        '& fieldset': { borderWidth: 1, borderColor: theme.palette.primary.main },
+        '& svg path': {
+          fill: theme.palette.primary.main,
+        },
+      },
+      '&:hover': {
+        '& fieldset': { borderColor: 'transparent' },
+      },
+      '& fieldset': {
+        borderColor: 'transparent',
+        borderWidth: 1,
+      },
     },
-  }),
-)(InputBase);
+    '& .MuiOutlinedInput-input': {
+      padding: '12px 14px 14px 5px',
+    },
+  },
+}))(TextField);
 
 
-export const Home = () => {
+export const Home = (): React.ReactElement => {
   const classes = useHomeStyles();
 
   return (
     <Container className={classes.wrapper} maxWidth="lg">
       <Grid container spacing={3}>
-        <Grid item xs={3}>
+        <Grid item sm={1} md={3}>
           <SideMenu classes={classes} /> 
         </Grid>
-        <Grid item xs={6}>
+        <Grid item sm={8} md={6}>
           <Paper className={classes.TweetsWrapper} style={{ height: '100%' }} variant="outlined">
-          <Paper className={classes.tweetsHeader} variant="outlined">
+            <Paper className={classes.tweetsHeader} variant="outlined">
               <Typography variant="h6">Главная</Typography>
             </Paper>
+            <Paper>
+              <div className={classes.addForm}>
+                <AddTweetForm classes={classes} />
+              </div>
+              <div className={classes.addFormBottomLine} />
+            </Paper>
+            <div>
+              {/* <CircularProgress /> */}
+            </div>
+
             {[
               ...new Array(20).fill(
                 <Tweet
@@ -143,8 +266,88 @@ export const Home = () => {
 
           </Paper>
         </Grid>
-        <Grid item xs={3}>
-          <SearchTextField fullWidth placeholder="Поиск в Твиттере" />
+        <Grid item sm={3} md={3}>
+          <div className={classes.rightSide}>
+            <SearchTextField
+              variant="outlined"
+              placeholder="Поиск по Твиттеру"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+              fullWidth
+            />
+            <Paper className={classes.rightSideBlock}>
+              <Paper className={classes.rightSideBlockHeader} variant="outlined">
+                <b>Актуальные темы</b>
+              </Paper>
+              <List>
+                <ListItem className={classes.rightSideBlockItem}>
+                  <ListItemText
+                    primary="Санкт-Петербург"
+                    secondary={
+                      <Typography component="span" variant="body2" color="textSecondary">
+                        Твитов: 3 331
+                      </Typography>
+                    }
+                  />
+                </ListItem>
+                <Divider component="li" />
+                <ListItem className={classes.rightSideBlockItem}>
+                  <ListItemText
+                    primary="#коронавирус"
+                    secondary={
+                      <Typography component="span" variant="body2" color="textSecondary">
+                        Твитов: 163 122
+                      </Typography>
+                    }
+                  />
+                </ListItem>
+                <Divider component="li" />
+                <ListItem className={classes.rightSideBlockItem}>
+                  <ListItemText
+                    primary="Беларусь"
+                    secondary={
+                      <Typography component="span" variant="body2" color="textSecondary">
+                        Твитов: 13 554
+                      </Typography>
+                    }
+                  />
+                </ListItem>
+                <Divider component="li" />
+              </List>
+            </Paper>
+            <Paper className={classes.rightSideBlock}>
+              <Paper className={classes.rightSideBlockHeader} variant="outlined">
+                <b>Кого читать</b>
+              </Paper>
+              <List>
+                <ListItem className={classes.rightSideBlockItem}>
+                  <ListItemAvatar>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="https://pbs.twimg.com/profile_images/1267938486566428673/US6KRPbA_normal.jpg"
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Dock Of Shame"
+                    secondary={
+                      <Typography component="span" variant="body2" color="textSecondary">
+                        @FavDockOfShame
+                      </Typography>
+                    }
+                  />
+                  <Button color="primary">
+                    <PersonAddIcon />
+                  </Button>
+                </ListItem>
+                <Divider component="li" />
+              </List>
+            </Paper>
+          </div>
         </Grid>
       </Grid>
     </Container>
