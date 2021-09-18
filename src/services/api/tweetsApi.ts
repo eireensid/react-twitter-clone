13d8bddd06e7c48b863d3a9303106dfbfbdca663
Irina <sidoren-irina@yandex.ru>
2021-09-18 16:57:43 +0300

    full tweet creation

--- src/services/api/tweetsApi.ts
+++ src/services/api/tweetsApi.ts
@@ -5,7 +5,7 @@ export const TweetsApi = {
   fetchTweets(): Promise<TweetsState['items']> {
     return axios.get('/tweets').then(({ data }) => data);
   },
-  fetchTweetData(id: string): Promise<Tweet> {
-    return axios.get('/tweets' + id).then(({ data }) => data);
+  fetchTweetData(id: string): Promise<Tweet[]> {
+    return axios.get('/tweets?_id=' + id).then(({ data }) => data);
   }
 }
